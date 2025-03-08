90b694c2068f20b583839feb4953b85480f69cec
VadymSnizhko <snizhko@gmail.com>
2025-03-08 19:45:23 +0200

    fourth task hw 07

--- js/task-4.js
+++ js/task-4.js
@@ -1,1 +1,24 @@
 
+const form = document.querySelector(".login-form");
+
+form.addEventListener("submit",handleForm);
+
+function handleForm(event){
+    event.preventDefault();
+
+    const elementsOnForm = event.target.elements;
+
+    const loginValue = {
+        email: elementsOnForm.email.value,
+        pass: elementsOnForm.password.value
+    }
+    if(loginValue.email.length && loginValue.pass.length){
+        console.log(loginValue); 
+    }else{
+        alert('All form fields must be filled in');
+    }
+
+    form.reset();
+
+}
+
